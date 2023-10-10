825e8ddbf3846acc5c00682ba67c2610dca732e9
dipan-22 <dipandutta944@gmail.com>
2023-10-10 20:08:16 +0530

    draft commit 1

--- pages/takedebaitregister.js
+++ pages/takedebaitregister.js
@@ -1,27 +1,92 @@
 import React from 'react'
+import styles from "../styles/audtion.module.scss";
 import style from '../styles/takedebaitregister.module.scss'
 import Navbar from "../components/Navbar/navbar";
 import Footer from "../components/Footer/footer";
+
 
 const  takedebaitregister = () => {
   return (
     <form>
         <div className={style.navbar3}><Navbar /></div>
-        <div className={style.Wrapper}>
-             <div className={style.bgimg}></div>
-             <div className={style.fields}>
-             <div className={style.header}>
-             <div className={style.text}></div>
-             <div className={style.underline}></div>
-             </div>
-                
-             <div className={style.input}>
-                
-             </div>
-             </div>
-             
-
+        <div className={style.wrapper}>
+          <div className={style.bgimg}></div>
+          <div className={style.fields}>
+            <div className={`${styles.formWrap} ${style.formWrap}`}>
+            <div className={style.circle}></div> 
+            <h1 className={style.leaderName}>Team Leader name</h1>
+            <div className={styles.inputGrp}>
+              <div className={styles.question}>Member 1: Phone No</div>
+              <input
+                type="text"
+                // value={data.member_1_number}
+                placeholder="Enter your answer"
+                // onChange={(e) => {
+                //   setData({ ...data, member_1_number: e.target.value });
+                // }}
+              />
+            </div>{" "}
+            <div className={styles.inputGrp}>
+              <div className={styles.question}>Member 1: Phone No</div>
+              <input
+                type="text"
+                // value={data.member_1_number}
+                placeholder="Enter your answer"
+                // onChange={(e) => {
+                //   setData({ ...data, member_1_number: e.target.value });
+                // }}
+              />
+            </div>{" "}
+            <div className={styles.inputGrp}>
+              <div className={styles.question}>Member 1: Phone No</div>
+              <input
+                type="text"
+                // value={data.member_1_number}
+                placeholder="Enter your answer"
+                // onChange={(e) => {
+                //   setData({ ...data, member_1_number: e.target.value });
+                // }}
+              />
+            </div>{" "}
+            <div className={styles.inputGrp}>
+              <div className={styles.question}>Member 1: Phone No</div>
+              <input
+                type="text"
+                // value={data.member_1_number}
+                placeholder="Enter your answer"
+                // onChange={(e) => {
+                //   setData({ ...data, member_1_number: e.target.value });
+                // }}
+              />
+            </div>{" "}
+            <div className={styles.inputGrp}>
+              <div className={styles.question}>Member 1: Phone No</div>
+              <input
+                type="text"
+                // value={data.member_1_number}
+                placeholder="Enter your answer"
+                // onChange={(e) => {
+                //   setData({ ...data, member_1_number: e.target.value });
+                // }}
+              />
+            </div>{" "}
+            <div className={styles.inputGrp}>
+              <div className={styles.question}>Member 1: Phone No</div>
+              <input
+                type="text"
+                // value={data.member_1_number}
+                placeholder="Enter your answer"
+                // onChange={(e) => {
+                //   setData({ ...data, member_1_number: e.target.value });
+                // }}
+              />
+            </div>{" "}
+            </div>
+            <button className={style.button}>Click here  to register</button>
+          
+          </div>
         </div>
+
         <Footer/>
     </form>
    
